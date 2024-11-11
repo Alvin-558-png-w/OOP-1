b1b5a46c52eb8047ce827cfa5ac095266c1de6c2
Alvin-558-png-w <alvin.nderui@gmail.com>
2024-11-11 10:15:10 +0300

    Initial OOP-1(afterclassproject)

---
+++ afterclassproject.py
@@ -0,0 +1,9 @@
+class robotI:
+    store="Codingal store"
+    name="Micky and Riley"
+    def details(self):
+     print("Our names are", self.name, "We were bought at the", self.store)
+obj=robotI()
+obj.details()
+
+
